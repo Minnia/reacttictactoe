55efa337871cf08938d92080d76806ffda2abbc4
Minnia <jasminestockholm@gmail.com>
2019-10-06 16:28:22 +0200

    init commit

---
+++ my-app/src/components/Game.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import Board from "./Board";
+
+class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null),
+          xIsNext: true
+        }
+      ]
+    };
+  }
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board"></div>
+        <div className="game-info">
+          <div>{/*status*/}</div>
+          <ol>{/*TODO*/}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Game;
